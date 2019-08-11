97fe14d152271d214aa18d4a286681caef180eac
Chou-Yu-Hsien <slps970093@gmail.com>
2019-08-11 19:09:14 +0800

    重構後

--- application/controllers/BuyControllerBefore.php
+++ application/controllers/BuyControllerBefore.php
@@ -24,50 +24,43 @@ class BuyController extends CI_Controller
 
         try {
             $this->db->trans_start();
+
             if (count($postData['items']) == 0) {
                 throw new Exception('請選擇產品');
             }
+
             $products = $this->ProductModel->getProductInByPrimaryKeys($postData['items']);
-            $orderNo = $this->createOrder($products, $postData);
-            $this->createOrderItem($orderNo, $products);
+
+            $total_price = collect($products)->sum('price');
+
+            $orderNo = $this->OrderModel->append([
+                'name' => $postData['name'],
+                'address' => $postData['address'],
+                'phone' => $postData['phone'],
+                'total_price' => $total_price
+            ], true);
+
+
+            foreach ($products as $row) {
+                $insertOrderItem[] = [
+                    'order_id' => $orderNo,
+                    'product_id' => $row['id'],
+                    'price' => $row['price']
+                ];
+            }
+
+            if (!is_array($insertOrderItem)) {
+                throw new Exception('沒有下訂單');
+            }
+
+            $this->OrderItemModel->multi_append($insertOrderItem);
+
             $this->db->trans_complete();
             redirect('buy/success/' . $orderNo);
         } catch (Exception $e) {
             $this->db->trans_rollback();
             redirect('buy?error=' . $e->getMessage());
         }
-    }
-
-    private function createOrder($products, $postData)
-    {
-
-        $total_price = collect($products)->sum('price');
-
-        $orderNo = $this->OrderModel->append([
-            'name' => $postData['name'],
-            'address' => $postData['address'],
-            'phone' => $postData['phone'],
-            'total_price' => $total_price
-        ], true);
-
-        return $orderNo;
-    }
-
-
-    private function createOrderItem($orderPrimaryKey, $products)
-    {
-        foreach ($products as $row) {
-            $insertOrderItem[] = [
-                'order_id' => $orderPrimaryKey,
-                'product_id' => $row['id'],
-                'price' => $row['price']
-            ];
-        }
-
-        if (!is_array($insertOrderItem)) {
-            throw new Exception('沒有下訂單');
-        }
-        $this->OrderItemModel->multi_append($insertOrderItem);
     }
 
     public function success($id)
